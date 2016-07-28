1858650cc446cbbde2d2a91f2c1b59703aef3dab
o-s-e <ose@recommind.com>
2016-07-28 14:58:20 +0200

    added demo ca

--- Docker/cfssl/entrypoint.sh
+++ Docker/cfssl/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 
-echo "Generating ca keys"
-exec cfssl \
-        genkey \
-        -initca /opt/ca.json | cfssljson ca
-        echo "Starting cfssl server"
-
+echo"Starting cfssl server"
 exec cfssl \
         serve \
         -config /opt/cfssl/config.json \
