31eadb72863c7c8a9e0abcda2158fb70664169a5
o-s-e <ose@recommind.com>
2016-07-28 16:15:57 +0200

    fixed cfssljson call

--- Docker/cfssl/entrypoint.sh
+++ Docker/cfssl/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
+
 echo"Starting cfssl server"
-exec cfssl \
-        serve \
-        -config /opt/cfssl/config.json \
-        -ca /opt/cfssl/ca.pem \
-        -ca-key /opt/cfssl/ca-key.pem \
-        $@
+exec cfssl serve -config /opt/cfssl/config.json -ca /opt/cfssl/ca.pem -ca-key /opt/cfssl/ca-key.pem $@
