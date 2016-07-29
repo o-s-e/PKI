7e9099dbd0fea977d9abe5c4aad51dab466c4858
o-s-e <ose@recommind.com>
2016-07-29 15:53:57 +0200

    initializing the client

--- Docker/cfssl/entrypoint.sh
+++ Docker/cfssl/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-
-
 echo "Starting cfssl server"
-exec cfssl serve -config /opt/cfssl/config.json -ca /opt/cfssl/ca.pem -ca-key /opt/cfssl/ca-key.pem $@
+exec cfssl serve -address 0.0.0.0 -config /opt/cfssl/config.json -ca /opt/cfssl/ca.pem -ca-key /opt/cfssl/ca-key.pem $@
---
+++ pki_client/cfssl_client.py
@@ -0,0 +1,72 @@
+import requests
+import os
+import socket
+import json
+import logging
+
+logger = logging.getLogger('cfssl_client')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+CERT_PATH = "/foo/bar"
+KEY_PATH = "/lar/far"
+CFSSL_URL = "http://192.168.99.100:8888"
+MODE = True
+
+
+def main():
+    cert_path = CERT_PATH
+    key_path = KEY_PATH
+    mode = MODE
+    cfssl_url = CFSSL_URL
+    common_name = get_hostname()
+    hosts = get_ip()
+    names = [{
+        "C" : "US",
+        "L" : "San Francisco",
+        "O" : "Example Company, LLC",
+        "OU": "Operations",
+        "ST": "California"
+    }]
+    logger.info('Starting')
+    if not os.path.exists(cert_path):
+        if not mode:
+            url = '{0}/api/v1/cfssl/newcert'.format(cfssl_url)
+
+            data = {
+                'request': {
+                    'CN'   : common_name,
+                    'names': names,
+                    'hosts': hosts,
+                }
+            }
+
+            response = requests.post(url, data=json.dumps(data)).json()
+
+            with open(cert_path, 'w') as fp:
+                fp.write(response['result']['certificate'])
+
+            with open(key_path, 'w') as fp:
+                fp.write(response['result']['private_key'])
+
+
+def get_ip():
+    for ip in socket.gethostbyname_ex(socket.gethostname())[2]:
+        if not ip.startswith("127."):
+            return ip
+
+
+def get_hostname():
+    return socket.getfqdn(socket.gethostname())
+
+
+def execute():
+    print get_ip()
+    print get_hostname()
+
+
+if __name__ == '__main__':
+    main()
